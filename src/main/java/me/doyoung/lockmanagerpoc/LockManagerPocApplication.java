455b1bb6cafb561ff55809949cd0c1167c5ffeb5
doyoung0205 <doyoung0205@naver.com>
2022-09-03 22:59:08 +0900

    Initial commit

---
+++ src/main/java/me/doyoung/lockmanagerpoc/LockManagerPocApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.lockmanagerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockManagerPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockManagerPocApplication.class, args);
+    }
+
+}
