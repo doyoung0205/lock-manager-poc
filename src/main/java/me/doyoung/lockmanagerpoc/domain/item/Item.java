e603c95aa12639ac816fd46d19634578d1cb84b5
doyoung0205 <doyoung0205@naver.com>
2022-09-12 10:16:46 +0900

    page

--- src/main/java/me/doyoung/lockmanagerpoc/domain/item/Item.java
+++ src/main/java/me/doyoung/lockmanagerpoc/domain/item/Item.java
@@ -20,10 +20,6 @@ public class Item {
     private String name;
     private int quantity;
 
-    private Item(String name) {
-        this.name = name;
-    }
-
     public Item(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
--- src/main/java/me/doyoung/lockmanagerpoc/interfaces/web/ItemWebController.java
+++ src/main/java/me/doyoung/lockmanagerpoc/interfaces/web/ItemWebController.java
@@ -2,14 +2,21 @@ package me.doyoung.lockmanagerpoc.interfaces.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("pages")
 public class ItemWebController {
 
     @RequestMapping("/items")
     public String routeItemListPage() {
-        return "items";
+        return "item/list";
+    }
+
+    @RequestMapping("/items/{itemId:[\\d]+}")
+    public String routeItemDetailPage(@PathVariable Long itemId) {
+        return "item/detail";
     }
 }
