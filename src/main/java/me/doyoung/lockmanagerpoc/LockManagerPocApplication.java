b01212e99c69898ea39e8740eeead22e1004d211
doyoung0205 <doyoung0205@naver.com>
2022-09-12 19:51:03 +0900

    feat: 상품 화면 추가

--- src/main/java/me/doyoung/lockmanagerpoc/LockManagerPocApplication.java
+++ src/main/java/me/doyoung/lockmanagerpoc/LockManagerPocApplication.java
@@ -1,13 +1,39 @@
 package me.doyoung.lockmanagerpoc;
 
+import me.doyoung.lockmanagerpoc.domain.item.Item;
+import me.doyoung.lockmanagerpoc.infra.ItemRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
 public class LockManagerPocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LockManagerPocApplication.class, args);
+
+
+    }
+
+    @Component
+    public static class ItemDataLoader implements ApplicationRunner {
+
+        private final ItemRepository itemRepository;
+
+        public ItemDataLoader(ItemRepository itemRepository) {
+            this.itemRepository = itemRepository;
+        }
+
+        public void run(ApplicationArguments args) {
+            itemRepository.saveAll(List.of(
+                    new Item("Mac Half Jacket [Cream]", 10),
+                    new Item("Raccoon Cable V Cardigan [Grey]", 10)
+            ));
+        }
     }
 
 }
--- src/main/java/me/doyoung/lockmanagerpoc/interfaces/web/ItemWebController.java
+++ src/main/java/me/doyoung/lockmanagerpoc/interfaces/web/ItemWebController.java
@@ -1,23 +1,30 @@
 package me.doyoung.lockmanagerpoc.interfaces.web;
 
-import lombok.RequiredArgsConstructor;
+import me.doyoung.lockmanagerpoc.application.ItemService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/pages")
 public class ItemWebController {
+    private final ItemService itemService;
+
+    public ItemWebController(ItemService itemService) {
+        this.itemService = itemService;
+    }
 
     @GetMapping("/items")
-    public String routeItemListPage() {
+    public String routeItemListPage(Model model) {
+        model.addAttribute("itemList", itemService.findAll());
         return "pages/items/list";
     }
 
     @GetMapping("/items/{itemId:\\d+}")
-    public String routeItemDetailPage(@PathVariable Long itemId) {
+    public String routeItemDetailPage(Model model, @PathVariable Long itemId) {
+        model.addAttribute("item", itemService.getItem(itemId));
         return "/pages/items/detail";
     }
 }
