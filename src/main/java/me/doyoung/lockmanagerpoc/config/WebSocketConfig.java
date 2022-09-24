9345b2cd2f3235a64f17eab0a688dd6e3497bdc0
dorie <dorie@dorie-29cm.local>
2022-09-24 20:34:35 +0900

    websocket move package config

--- src/main/java/me/doyoung/lockmanagerpoc/config/WebSocketConfig.java
+++ src/main/java/me/doyoung/lockmanagerpoc/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
-package me.doyoung.lockmanagerpoc.interfaces.socket;
+package me.doyoung.lockmanagerpoc.config;
 
 
+import me.doyoung.lockmanagerpoc.interfaces.socket.LockHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -20,4 +21,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(lockHandler, "ws/lock").setAllowedOrigins("*");
     }
+
+    //.withSockJS() 추가
+    //setAllowedOrigins("*")에서 *라는 와일드 카드를 사용하면
+    //보안상의 문제로 전체를 허용하는 것보다 직접 하나씩 지정해주어야 한다고 한다.
+
 }
